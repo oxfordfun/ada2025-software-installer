f6c3d03748f392fddc9bee3f66cf1915277100a5
Jonah Loughlin <Jonah.Loughlin@stfc.ac.uk>
2023-08-25 11:37:58 +0100

    Buttons and version info

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import webbrowser
 import flask
 import os
 import requests
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
+from distutils.version import StrictVersion
+
 
 def str_to_bool(str):
     if str == "True":
         return True
     return False
+
 
 HOST = os.getenv("ADA2025_SI_HOST") or "127.0.0.1"
 PORT = os.getenv("ADA2025_SI_PORT") or 7322
@@ -17,18 +20,30 @@ FS_URL = os.getenv("ADA2025_SI_FS_URL") or "https://ada-files.oxfordfun.com/soft
 
 app = flask.Flask(__name__)
 
+
 @app.route("/")
 def myapp():
-    software_list = get_software_list()
-    return flask.render_template("app.jinja2", software_list=software_list)
+    software_info = get_software_info()
+    return flask.render_template("app.jinja2", software_info=software_info)
+
 
 @app.route("/search")
 def search():
-    software_list = get_software_list()
     search_term = flask.request.args["search"]
+    software_info = get_software_info(search_term)
+    return flask.render_template("app.jinja2", software_info=software_info)
+
+
+def get_software_info(search_term=None):
+    software_list = get_software_list()
     if search_term:
         software_list = find_items_with_string(software_list, search_term)
-    return flask.render_template("app.jinja2", software_list=software_list)
+    version_list = get_all_latest_software_versions(software_list)
+    software_info = []
+    for i in range(0, len(software_list)):
+        software_info.append([software_list[i], version_list[i]])
+    return software_info
+
 
 def find_items_with_string(arr, search_string):
     result = []
@@ -37,14 +52,15 @@ def find_items_with_string(arr, search_string):
             result.append(item)
     return result
 
+
 def get_software_list():
     response = requests.get(FS_URL)
     softwares = []
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            if href.endswith('/'):
+        soup = BeautifulSoup(response.content, "html.parser")
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            if href.endswith("/"):
                 softwares.append(href)
         softwares.remove("misc/")
         softwares = softwares[1:]
@@ -52,8 +68,38 @@ def get_software_list():
         print(f"Error: Unable to retrieve content from {FS_URL}")
     return softwares
 
+
+def get_all_latest_software_versions(software_list):
+    latest_versions = []
+    for software in software_list:
+        response = requests.get(FS_URL + f"/{software}")
+        if response.status_code == 200:
+            latest_versions.append(get_latest_software_version(response, software))
+    return latest_versions
+
+
+def get_latest_software_version(response, software_name):
+    all_versions = []
+    soup = BeautifulSoup(response.content, "html.parser")
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        if href.endswith("/"):
+            all_versions.append(href)
+
+    prefix = software_name + "-"
+    filtered_versions = [
+        version[len(prefix) :] for version in all_versions if version.startswith(prefix)
+    ]
+
+    if not filtered_versions:
+        return "N/A"
+
+    return max(filtered_versions, key=StrictVersion)
+
+
 def main():
     app.run(host=HOST, port=PORT, debug=DEBUG)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
